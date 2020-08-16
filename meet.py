36c13ce62bc597ff1d8f8e17bef33d889bd654e2
Prajwal Karale <54241917+PrajwalKarale@users.noreply.github.com>
2020-08-16 12:32:53 +0530

    Update meet.py

--- meet.py
+++ meet.py
@@ -1,11 +1,14 @@
+#importing the necessary library..
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get("http://www.gmail.com")
 browser.implicitly_wait(60)
 browser.maximize_window()
+
 email = browser.find_element_by_name("identifier")
 email.clear()
 email.send_keys("prajwalkarale0508@student.sfit.ac.in")
+
 # browser.find_element_by_xpath("//*[@id='identifierNext']/div/button/div[2]").click()
 browser.implicitly_wait(60)
 
@@ -15,9 +18,13 @@ password = browser.find_element_by_name("password").send_keys("08061999praj")
 
 browser.find_element_by_id('passwordNext').click()
 
+#to open the meet dialog box.
 enter_code = browser.find_element_by_xpath('//*[@id=":jp"]').click()
 
-browser.find_element_by_class_name('YK').send_keys('BECMPNA')
+meeting_code = input('ENTER THE MEETING CODE: ')
 
-# //*[@id=":jp"]
-# /html/body/div[24]/div[2]/input
+#to type the meeting code.
+browser.find_element_by_class_name('YK').send_keys(meeting_code)
+
+#to click the JOIN button.
+browser.find_element_by_xpath('/html/body/div[23]/div[3]/button').click()
